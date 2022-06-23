3eb309c3232e4817fad7b2996d220a1fb9517c49
Omar Bitar <o.bitar97@gmail.com>
2022-06-23 10:24:26 +0300

    updated controller for book creation with default url

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -4,11 +4,29 @@
 namespace app\controllers;
 
 use app\models\Book;
+use Yii;
 
 class BookController extends \yii\web\Controller
 {
     public function actionIndex()
     {
         return $this->asJson(Book::find()->all());
+    }
+
+    public function actionPublish()
+    {
+        $request = Yii::$app->request;
+
+        $book = new Book();
+        $book->attributes = 
+        [
+            "name"                  => $request->post('name'),
+            "author"                => $request->post('author'),
+            "release_year"          => $request->post('release_year'),
+            "is_available_for_loan" => $request->post('is_available_for_loan')
+        ];
+        $book->save();
+
+        return $this->asJson(['created: ',$book]);
     }
 }
